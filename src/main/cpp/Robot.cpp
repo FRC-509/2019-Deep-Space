a39070fcfb5b717e6f50df9539e5ca339ca47b7b
adit-ya <42247145+adit-ya@users.noreply.github.com>
2019-02-07 18:11:25 -0500

    Code tested at end of week 4 on Saturday, Feb 2nd.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -32,60 +32,84 @@
 class Robot : public frc::TimedRobot {
 
 public:
+  
 
 //Constructing joystick objects
- frc::Joystick r_stick{0};
- frc::Joystick l_stick{1};
- frc::Joystick logicontroller{2};
+  frc::Joystick l_stick{0};
+  frc::Joystick r_stick{1};
+  frc::Joystick logicontroller{2};
 
 //Constructing motor controller objects (Spark Max)    
- rev::CANSparkMax m_lr{0, rev::CANSparkMax::MotorType::kBrushless};
- rev::CANSparkMax m_lf{1, rev::CANSparkMax::MotorType::kBrushless};
- rev::CANSparkMax m_rr{2, rev::CANSparkMax::MotorType::kBrushless};
- rev::CANSparkMax m_rf{3, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_lr{0, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_lf{1, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_rr{2, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_rf{3, rev::CANSparkMax::MotorType::kBrushless};
 
 //Constructing motor controller objects (Talon SRX)
- WPI_TalonSRX * m_rightelevator = new WPI_TalonSRX{4};
- WPI_TalonSRX * m_leftelevator = new WPI_TalonSRX{5};
+//modified numbers
+  WPI_TalonSRX * m_rightelevator = new WPI_TalonSRX{11};
+  WPI_TalonSRX * m_leftelevator = new WPI_TalonSRX{12};
+  WPI_TalonSRX * m_arm1 = new WPI_TalonSRX{2};
+  WPI_TalonSRX * m_arm2 = new WPI_TalonSRX{3};
+  WPI_TalonSRX * m_intake = new WPI_TalonSRX{13};
+  //WPI_TALONSRX
+  
+  
+
 
 //Instantiating the compressor
- frc::Compressor *comp = new frc::Compressor(0);
+  frc::Compressor *comp = new frc::Compressor(0);
 
 //Construct Double Solenoid object
- frc::DoubleSolenoid GearSol {2, 3};
+  frc::DoubleSolenoid panelSol {0, 1};
+  frc::DoubleSolenoid shiftSol {2, 3}; //was 4,5 previously 
 
 //Setting encoder to corresponding motors
- rev::CANEncoder rf_encoder = m_rf.GetEncoder();
- rev::CANEncoder lf_encoder = m_lf.GetEncoder();
+  rev::CANEncoder rf_encoder = m_rf.GetEncoder();
+  rev::CANEncoder lf_encoder = m_lf.GetEncoder();
 
  //PIDController for the right front and left front motors
- rev::CANPIDController rf_pidController = m_rf.GetPIDController();
- rev::CANPIDController rr_pidController = m_rr.GetPIDController();
+  rev::CANPIDController rf_pidController = m_rf.GetPIDController();
+  rev::CANPIDController rr_pidController = m_rr.GetPIDController();
 
- //frc::DigitalInput climberlimit = new frc::DigitalInput(6);
+  frc::DigitalInput *elevLimitBottom = new frc::DigitalInput(0);
+  frc::DigitalInput *elevLimitTop = new frc::DigitalInput(1);
 
 //Arcade Drive object
- frc::DifferentialDrive m_arcadeDrive{m_lf, m_rf};
+  frc::DifferentialDrive m_arcadeDrive{m_lf, m_rf};
 
 //rval and lval are variables that will be used to store joystick values
- float rval= 0;
- float lval= 0;
+  float rval= 0;
+  float lval= 0;
 
- //PID variables
- double kP = 0.1, kI = 1e-4, kD = 1, kIz = 0, kFF = 0, kMaxOutput = 1, kMinOutput = -1;
- const double MaxRPM = 5676;
+//PID variables
+  double kP = 0.1, kI = 1e-4, kD = 1, kIz = 0, kFF = 0, kMaxOutput = 1, kMinOutput = -1;
+  const double MaxRPM = 5676;
+//Zach's Far Superior PID Varriables
+  double integratState, integratMax, integratMin;
+  double integratGain, propGain;
+//Is the grabber out or in?
+  bool out;
 
 
 
   void RobotInit() {
-     //Setting SetClosedLoopControl to true turns the Compressor on 
+    // Setting the grabber so that the piston is in and changing the value of bool out to reflect that
+    panelSol.Set(frc::DoubleSolenoid::Value::kReverse);
+    out=0;
+
+
+    
+  //Setting SetClosedLoopControl to true turns the Compressor on 
      comp->SetClosedLoopControl(true);
      
      //Adding camera using IP address
      cs::AxisCamera ipCamera = frc::CameraServer::GetInstance()->AddAxisCamera("10.5.9.53");
+
+     cs::UsbCamera usbcamera = frc::CameraServer::GetInstance()->StartAutomaticCapture();
      
      //Start auto capture of images to send to dashboard
-     frc::CameraServer::GetInstance()->StartAutomaticCapture();
+     //frc::CameraServer::GetInstance()->StartAutomaticCapture();
 
     // TODO: Setup this runner to run it its own thread
     // Uses a lambda function -- Check these out. Newer C++ 11 feature.
@@ -121,6 +145,19 @@ public:
   }
 
   void TeleopPeriodic()  {
+    //int val= -r_stick.GetY();
+    //double PIDSetPoint=500;
+    //double error=PIDSetPoint-rf_encoder.GetVelocity();
+    //m_rr.Set(updatePID(error));
+
+ 
+    
+
+    Elevator();
+    Arm();
+    if (logicontroller.GetRawButtonPressed(1)) { ToggleGrabber(); }
+
+
 
     // Setting PID coeffiencients for Zach's manual program
     // PID(-2000,-2000,0.000038,0,0);
@@ -168,16 +205,22 @@ void WestCoastDrive() {
 
      rval= -r_stick.GetY();
      lval= l_stick.GetY();
+     if (rval<0.01 && rval>-0.01){
+       rval=0;
+       }
+      if (lval<0.01 && lval>-0.01){
+       lval=0;
+       }
 
      m_rf.Set(rval);
      m_rr.Set(rval);
      m_lf.Set(lval);
      m_lr.Set(lval);
 
-     if (l_stick.GetRawButton(2)) {
-       GearSol.Set(frc::DoubleSolenoid::Value::kForward);
+     if (r_stick.GetRawButton(1)) {
+       shiftSol.Set(frc::DoubleSolenoid::Value::kForward);
      } else {
-       GearSol.Set(frc::DoubleSolenoid::Value::kReverse);
+       shiftSol.Set(frc::DoubleSolenoid::Value::kReverse);
      }
  }
 
@@ -193,37 +236,70 @@ void WestCoastDrive() {
  }
 
 //Function for Elevator
- void Elevator() {
-   m_rightelevator->Set(logicontroller.GetRawAxis(3));
-   m_leftelevator->Set(-logicontroller.GetRawAxis(3));
-  //  while (climberlimit.Get()) {
-  //   m_rightelevator->Set(0);
-  //   m_leftelevator->Set(0);
-  //  }
+//positive setPoint indicates upwards direction
+  void Elevator() {
+    float setPoint=.1+floor(-logicontroller.GetRawAxis(3)*1000/1.5)/1000; 
+    //float setPoint=0.1;
+
+    //limit switch value of 1 eqals open
+    frc::SmartDashboard::PutNumber("logicontroller", setPoint);
+
+    if (elevLimitBottom->Get() && setPoint < 0) {
+     setPoint=0;
+    }
+    if (elevLimitTop->Get() && setPoint > 0) {
+     setPoint=0;
+    }
+    m_rightelevator->Set(setPoint);
+    m_leftelevator->Set(-setPoint);
+    frc::SmartDashboard::PutNumber("elevLimitBottom", (int) elevLimitBottom->Get());
+    
  }
 
-// The following functions are part of Zach's manual attempt at PID
-  /*float PIDCalc(float expected, float actual, float Kp, float Ki, float Kd){
-    errorP=expected-actual;
-    errorDeriv=errorP-errorLast;
-    errorInt=errorP+errorInt;
-    output=Kp*errorP+Ki*errorInt+Kd*errorDeriv;
-    return output;
-  }
-  void motorSet(float LSet, float RSet){
-    m_lf->Set(LSet); 
-    m_lr->Set(LSet);
-    m_rf->Set(RSet); 
-    m_rr->Set(RSet);
-  }
-  void PID(float LSpeed, float RSpeed, float Kp, float Ki, float Kd){
-    if(boolPID=true){
-      RateLeft=lf_encoder.GetRate();
-      RateRight=-rf_encoder.GetRate();
-      LSet=PIDCalc(-LSpeed, RateLeft,Kp,Ki,Kd);
-      RSet=PIDCalc(RSpeed, RateRight,Kp,Ki,Kd);
-    }*/
+ void Arm() {
+    float setPoint=-0.07+floor(logicontroller.GetRawAxis(1)/3*1000)/1000; 
+    //limit switch value of 1 eqals open
 
+    frc::SmartDashboard::PutNumber("Arm Logic Controller", setPoint);
+    m_arm1->Set(setPoint);
+    m_arm2->Set(setPoint);
+
+    int i=0;
+    if (logicontroller.GetRawButton(6)){
+      i=1;
+    }
+    if (logicontroller.GetRawButton(5)){
+      i=-1;
+    }
+    m_intake->Set(i*0.75);
+
+ }
+
+ void ToggleGrabber() {
+     if (out){
+        panelSol.Set(frc::DoubleSolenoid::Value::kReverse);
+        out=0;
+     }else{
+        panelSol.Set(frc::DoubleSolenoid::Value::kForward);
+        out=1;
+     }
+ }
+
+//  void lifter() {
+
+//  }
+ 
+ 
+
+// The following functions are part of Zach's manual attempt at PID
+  
+  double updatePID(double error){
+    double pTerm, dTerm, iTerm;
+    integratState += error;
+    pTerm=propGain*error;
+    iTerm=integratGain*integratState;
+    return pTerm+iTerm;
+  }
 
   };
 
